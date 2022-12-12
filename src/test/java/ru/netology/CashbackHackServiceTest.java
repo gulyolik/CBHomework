f9428f145ef7a201151672dbd156d57454f51114
gulyolik <olia.guliaeva@yandex.ru>
2022-12-13 00:01:21 +0300

    Final Junit commit

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -9,18 +9,18 @@ public class CashbackHackServiceTest {
 @Rule
     @Test
     public void shouldCount1Test() {
-        Assert.assertEquals(service.remain(1), 999);
+        Assert.assertEquals(service.remain(999), 1);
 
     }
 
     @Test
     public void shouldCount2Test() {
-        Assert.assertEquals(service.remain(2), 998);
+        Assert.assertEquals(service.remain(998), 2);
     }
 
     @Test
     public void shouldCount3Test() {
-        Assert.assertEquals(service.remain(3), 997);
+        Assert.assertEquals(service.remain(997), 3);
     }
 
     @Test
@@ -30,25 +30,26 @@ public class CashbackHackServiceTest {
 
     @Test
     public void shouldCount999Test() {
-        Assert.assertEquals(service.remain(999), 1);
+        Assert.assertEquals(service.remain(1), 999);
     }
 
     @Test
     public void shouldCount998Test() {
-        Assert.assertEquals(service.remain(998), 2);
+        Assert.assertEquals(service.remain(2), 998);
     }
 
     @Test
     public void shouldCount997Test() {
-        Assert.assertEquals(service.remain(997), 3);
+        Assert.assertEquals(service.remain(3), 997);
     }
     @Test
     public void shouldCheckBoarders(){
-        Assert.assertEquals(service.remain(-1), 1001);
+        Assert.assertEquals( -1,service.remain(1001));
     }
 
     @Test
     public void shouldCheckBoarder(){
-        Assert.assertEquals(service.remain(0),1000);
+        Assert.assertEquals(0,service.remain(1000));
     }
+
 }
