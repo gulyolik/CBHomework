a63232f6f5bb106df50d4f297bedf952626541a5
gulyolik <olia.guliaeva@yandex.ru>
2022-12-12 23:50:26 +0300

    JUnit4 commit

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,63 +1,54 @@
 package ru.netology;
 
-import org.testng.annotations.Test;
-import org.testng.Assert;
+import org.junit.Assert;
+import org.junit.Test;
 
-import java.util.concurrent.Callable;
+import static ru.netology.Rule.service;
 
-class CashbackHackServiceTest {
-
+public class CashbackHackServiceTest {
+@Rule
     @Test
     public void shouldCount1Test() {
-        CashbackHackService service = new CashbackHackService();
         Assert.assertEquals(service.remain(1), 999);
 
     }
 
     @Test
     public void shouldCount2Test() {
-        CashbackHackService service = new CashbackHackService();
         Assert.assertEquals(service.remain(2), 998);
     }
 
     @Test
     public void shouldCount3Test() {
-        CashbackHackService service = new CashbackHackService();
         Assert.assertEquals(service.remain(3), 997);
     }
 
     @Test
     public void shouldCount500Test() {
-        CashbackHackService service = new CashbackHackService();
         Assert.assertEquals(service.remain(500), 500);
     }
 
     @Test
     public void shouldCount999Test() {
-        CashbackHackService service = new CashbackHackService();
         Assert.assertEquals(service.remain(999), 1);
     }
 
     @Test
     public void shouldCount998Test() {
-        CashbackHackService service = new CashbackHackService();
         Assert.assertEquals(service.remain(998), 2);
     }
 
     @Test
     public void shouldCount997Test() {
-        CashbackHackService service = new CashbackHackService();
         Assert.assertEquals(service.remain(997), 3);
     }
     @Test
     public void shouldCheckBoarders(){
-        CashbackHackService service = new CashbackHackService();
         Assert.assertEquals(service.remain(-1), 1001);
     }
 
     @Test
     public void shouldCheckBoarder(){
-        CashbackHackService service = new CashbackHackService();
         Assert.assertEquals(service.remain(0),1000);
     }
 }
