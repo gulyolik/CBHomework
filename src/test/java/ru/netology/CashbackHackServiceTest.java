374c3ba32c1e5b12d65d0dc7725478132e45de69
gulyolik <olia.guliaeva@yandex.ru>
2022-12-16 21:37:45 +0300

    JUnit4-platform commit

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -2,6 +2,7 @@ package ru.netology;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import static ru.netology.Rule.service;
 
@@ -9,42 +10,38 @@ public class CashbackHackServiceTest {
 @Rule
     @Test
     public void shouldCount1Test() {
-        Assert.assertEquals(service.remain(999), 1);
+        Assert.assertEquals( 1, service.remain(999));
 
     }
 
     @Test
     public void shouldCount2Test() {
-        Assert.assertEquals(service.remain(998), 2);
+        Assert.assertEquals( 2,service.remain(998));
     }
 
     @Test
     public void shouldCount3Test() {
-        Assert.assertEquals(service.remain(997), 3);
+        Assert.assertEquals(3, service.remain(997));
     }
 
     @Test
     public void shouldCount500Test() {
-        Assert.assertEquals(service.remain(500), 500);
+        Assert.assertEquals( 500, service.remain(500));
     }
 
     @Test
     public void shouldCount999Test() {
-        Assert.assertEquals(service.remain(1), 999);
+        Assert.assertEquals( 999,service.remain(1));
     }
 
     @Test
     public void shouldCount998Test() {
-        Assert.assertEquals(service.remain(2), 998);
+        Assert.assertEquals( 998, service.remain(2));
     }
 
     @Test
     public void shouldCount997Test() {
-        Assert.assertEquals(service.remain(3), 997);
-    }
-    @Test
-    public void shouldCheckBoarders(){
-        Assert.assertEquals( -1,service.remain(1001));
+        Assert.assertEquals( 997,service.remain(3));
     }
 
     @Test
@@ -52,4 +49,46 @@ public class CashbackHackServiceTest {
         Assert.assertEquals(0,service.remain(1000));
     }
 
+    @Test
+    public void shouldCount1TestJupiter(){
+        Assertions.assertEquals(1,service.remain(999));
+    }
+
+    @Test
+    public void shouldCount2TestJupiter(){
+    Assertions.assertEquals(2, service.remain(998));
+    }
+
+    @Test
+    public void shouldCount3TestJupiter(){
+    Assertions.assertEquals(3,service.remain(997));
+    }
+
+
+    @Test
+    public void shouldCount500TestJupiter(){
+    Assertions.assertEquals(500, service.remain(500));
+    }
+
+    @Test
+    public void shouldCount999TestJupiter(){
+    Assertions.assertEquals(999, service.remain(1));
+    }
+
+    @Test
+    public void shouldCount998TestJupiter(){
+    Assertions.assertEquals(998, service.remain(2));
+    }
+
+    @Test
+    public void shouldCount997TestJupiter(){
+    Assertions.assertEquals(997, service.remain(3));
+    }
+
+    @Test
+    public void shouldCheckTheBoardersTestJupiter(){
+    Assertions.assertEquals(0, service.remain(1000));
+    }
+
 }
+
